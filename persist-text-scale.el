d029254aef58c78a9c332318817f08f5436768ea
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-03-26 23:30:56 -0400

    Enhance docstrings and comments

--- persist-text-scale.el
+++ persist-text-scale.el
@@ -41,47 +41,60 @@
 (defcustom persist-text-scale-file (expand-file-name "persist-text-scale"
                                                      user-emacs-directory)
   "File where the persist-text-scale data is stored.
-
 This file holds the data for persisting the text scale across sessions.
 It can be customized to a different file path as needed."
   :type 'file
   :group 'persist-text-scale)
 
 (defcustom persist-text-scale-restore-once t
-  "If non-nil, only restore text scale once per buffer.
-Typically when the buffer is loaded."
+  "If non-nil, restore text scale only once per buffer.
+When non-nil, the text scale will be restored either when the buffer is loaded
+or when the buffer is displayed in a window for the first time. Subsequent
+window changes will not trigger additional restoration.
+
+When nil, text-scale will always be restored."
   :type 'boolean
   :group 'persist-text-scale)
 
 (defcustom persist-text-scale-verbose nil
-  "If non-nil, display messages during text scale restoration."
+  "If non-nil, display informative messages during text scale restoration.
+These messages will indicate when and how the text scale was restored, aiding
+in debugging or monitoring behavior."
   :type 'boolean
   :group 'persist-text-scale)
 
 (defcustom persist-text-scale-buffer-type-function nil
-  "Optional function to override the buffer type determination.
-This overrides the function `persist-text-scale--buffer-type'. If non-nil, this
-function will be called to determine the buffer type name. It should return a
-string representing the buffer type name or nil to use the default behavior
-based on `major-mode`."
+  "Optional function to customize buffer type classification.
+If non-nil, this function overrides `persist-text-scale--buffer-type' and is
+invoked to determine the buffer type identifier used for text scale grouping.
+It must return a string or symbol representing the buffer type, or nil to fall
+back to the default classification."
   :type '(choice (const :tag "None" nil) function)
   :group 'persist-text-scale)
 
 (defcustom persist-text-scale-autosave-interval (* 7 60)
-  "The interval between autosaves of persist text scale data.
-If set to nil, disables timer-based autosaving."
+  "Time interval, in seconds, between automatic saves of text scale data.
+If set to an integer value, enables periodic autosaving of persisted text scale
+information at the specified interval.
+If set to nil, disables timer-based autosaving entirely."
   :type '(choice (const :tag "Disabled" nil)
-                 (integer :tag "Seconds")))
+                 (integer :tag "Seconds"))
+  :group 'persist-text-scale)
 
 ;;; Variables
 
 (defvar persist-text-scale--data nil
-  "Alist to store text scale per major mode.")
+  "Alist mapping buffer identifiers to their corresponding text scale amount.
+Each entry associates either a file path or a buffer type name with the numeric
+value of the text scale applied to that buffer or group.")
 
-(defvar persist-text-scale--previous-text-scale 1
-  "Last text scale that the user has chosen.")
+(defvar persist-text-scale--previous-text-scale-amount 0
+  "Most recent text scale amount selected by the user.
+This value reflects the numeric text scale adjustment applied in the last
+interactive text scale change and is used internally to support restoration.")
 
 (defvar persist-text-scale--timer nil)
+
 (defvar-local persist-text-scale--restored-p nil)
 (defvar-local persist-text-scale--restored-amount nil)
 
@@ -96,8 +109,7 @@ If set to nil, disables timer-based autosaving."
 (defun persist-text-scale--manage-timer ()
   "Set or cancel an invocation of `persist-text-scale-autosave' on a timer.
 If `persist-text-scale-mode' is enabled, set the timer, otherwise cancel the
-timer. This should not cause noticeable delays for users --
-`persist-text-scale-autosave' executes in under 5 ms on my system."
+timer."
   (when (boundp 'persist-text-scale-autosave-interval)
     (if (and (bound-and-true-p persist-text-scale-mode)
              persist-text-scale-autosave-interval
@@ -152,7 +164,7 @@ Returns nil when the buffer type is nil."
   (let ((mode (persist-text-scale--buffer-type)))
     (when (and mode persist-text-scale--data)
       (let ((scale (or (cdr (assoc mode persist-text-scale--data))
-                       persist-text-scale--previous-text-scale)))
+                       persist-text-scale--previous-text-scale-amount)))
         (when scale
           (let ((amount scale))
             amount))))))
@@ -175,7 +187,7 @@ alist."
           (push (cons buffer-type text-scale-mode-amount)
                 persist-text-scale--data))
 
-        (setq persist-text-scale--previous-text-scale text-scale-mode-amount)))))
+        (setq persist-text-scale--previous-text-scale-amount text-scale-mode-amount)))))
 
 (defun persist-text-scale-restore (&optional object &rest _)
   "Restore the text scale for the current buffer .
@@ -248,7 +260,7 @@ If a text scale value is found, it sets the text scale using `text-scale-set'."
       (indent-region (point-min) (point-max)))))
 
 (defun persist-text-scale-load ()
-  "Save data from `persist-text-scale-file'."
+  "Load data from `persist-text-scale-file'."
   (load persist-text-scale-file t t t))
 
 ;;; Mode
@@ -257,7 +269,7 @@ If a text scale value is found, it sets the text scale using `text-scale-set'."
 (define-minor-mode persist-text-scale-mode
   "Toggle `persist-text-scale-mode'."
   :global t
-  :lighter " PerTScale"
+  :lighter " PTScale"
   :group 'persist-text-scale
   (if persist-text-scale-mode
       (progn
@@ -266,12 +278,7 @@ If a text scale value is found, it sets the text scale using `text-scale-set'."
         (add-hook 'kill-emacs-hook #'persist-text-scale-save)
         (add-hook 'window-buffer-change-functions #'persist-text-scale-restore 99)
         (add-hook 'find-file-hook #'persist-text-scale-restore 99)
-        (add-hook 'text-scale-mode-hook #'persist-text-scale-persist 99)
-
-        ;; Put this temporarily
-        ;; (add-hook 'window-configuration-change-hook #'persist-text-scale-restore)
-        ;; (add-hook 'window-state-change-hook #'persist-text-scale-restore)
-        )
+        (add-hook 'text-scale-mode-hook #'persist-text-scale-persist 99))
     (persist-text-scale--cancel-timer)
     (remove-hook 'kill-emacs-hook #'persist-text-scale-save)
     (remove-hook 'window-buffer-change-functions #'persist-text-scale-restore)
