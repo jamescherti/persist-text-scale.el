8098af6ba9ff251d19f9e41b39c84f7b9b758f26
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-01-31 12:43:07 -0500

    Release: 1.0.4

--- tests/test-persist-text-scale.el
+++ tests/test-persist-text-scale.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2025-2026 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 1.0.3
+;; Version: 1.0.4
 ;; URL: https://github.com/jamescherti/persist-text-scale.el
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "24.1"))
